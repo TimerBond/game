61b25918a725a914296e2796d48c3974c22c7c8a
TimerBond <45458954+TimerBond@users.noreply.github.com>
2019-02-18 17:31:45 +0300

    Add files via upload

--- pygame/anim.py
+++ pygame/anim.py
@@ -25,8 +25,10 @@ class AnimatedSprite(pygame.sprite.Sprite):
         self.cur_frame = 0
         self.image = self.frames[self.cur_frame]
         self.rect = self.rect.move(x, y)
+        print(self.frames)
 
     def cut_sheet(self, sheet, columns, rows):
+
         self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
                                 sheet.get_height() // rows)
         for j in range(rows):
@@ -35,25 +37,48 @@ class AnimatedSprite(pygame.sprite.Sprite):
                 self.frames.append(sheet.subsurface(pygame.Rect(
                     frame_location, self.rect.size)))
 
+
     def update(self):
         self.cur_frame = (self.cur_frame + 1) % len(self.frames)
         self.image = self.frames[self.cur_frame]
 
+
 pygame.init()
-size = width, height = 240, 120
+size = width, height = 360, 120
 screen = pygame.display.set_mode(size)
 all_sprites = pygame.sprite.Group()
-afk = AnimatedSprite(load_image('anim1.png'), 8, 1, 0, 0)
-run = AnimatedSprite(load_image('anim2.png'), 9, 1, 120, 0)
+move = AnimatedSprite(load_image('anim1.png'), 8, 1, 0, 0)
 clock = pygame.time.Clock()
+t = 0
+b = False
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYUP:
+            all_sprites.empty()
+            move = AnimatedSprite(load_image('anim1.png'), 8, 1, 0, 0)
+            b = False
+        if event.type == pygame.KEYDOWN:
+            all_sprites.empty()
+            if event.key == 275:
+                move = AnimatedSprite(load_image('anim2.png'), 9, 1, 0, 0)
+                print('run')
+            if event.key == 32:
+                move = AnimatedSprite(load_image('anim3.png'), 7, 1, 0, 0)
+                b = True
+                print('jump')
     screen.fill(pygame.Color('white'))
+    if b:
+        t += 1
+    if t == 7:
+        t = 0
+        b = False
+        all_sprites.empty()
+        move = AnimatedSprite(load_image('anim1.png'), 8, 1, 0, 0)
     all_sprites.draw(screen)
     all_sprites.update()
-    clock.tick(15)
+    clock.tick(10)
     pygame.display.flip()
 pygame.quit()
