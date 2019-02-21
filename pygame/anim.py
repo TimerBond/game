b66aae167dd06c9f6f9d77777d1ba25f037f6c64
TimerBond <45458954+TimerBond@users.noreply.github.com>
2019-02-21 17:30:12 +0300

    Add files via upload

--- pygame/anim.py
+++ pygame/anim.py
@@ -1,37 +1,14 @@
-import os, pygame
+import pygame
 
-
-def load_image(name, colorkey=None):
-    fullname = os.path.join('data', name)
-    try:
-        image = pygame.image.load(fullname)
-    except pygame.error as message:
-        print('Cannot load image:', name)
-        raise SystemExit(message)
-    image = image.convert_alpha()
-
-    if colorkey is not None:
-        if colorkey is -1:
-            colorkey = image.get_at((0, 0))
-        image.set_colorkey(colorkey)
-    return image
-
-def load_sound(name):
-    fullname = os.path.join('data', name)
-    try:
-        sound = pygame.mixer.music.load(fullname)
-    except pygame.error as message:
-        print('Cannot load image:', name)
-        raise SystemExit(message)
-
-class AnimatedSprite(pygame.sprite.Sprite):
-    def __init__(self, sheet, columns, rows, x, y):
+def __init__(self, sheet, columns, rows, x, y, all_sprites):
         super().__init__(all_sprites)
         self.frames = []
         self.cut_sheet(sheet, columns, rows)
         self.cur_frame = 0
         self.image = self.frames[self.cur_frame]
-        self.rect = self.rect.move(x, y)
+        self.rect = self.image.get_rect().move(
+            x, y - self.rect.h + CELL_SIZE + 3
+        )
 
     def cut_sheet(self, sheet, columns, rows):
 
@@ -47,45 +24,3 @@ class AnimatedSprite(pygame.sprite.Sprite):
     def update(self):
         self.cur_frame = (self.cur_frame + 1) % len(self.frames)
         self.image = self.frames[self.cur_frame]
-
-pygame.init()
-size = width, height = 120, 120
-screen = pygame.display.set_mode(size)
-all_sprites = pygame.sprite.Group()
-jump_sprite = pygame.sprite.Group()
-move = AnimatedSprite(load_image('anim1.png'), 8, 1, 0, 0)
-clock = pygame.time.Clock()
-load_sound('minecraft-standartnaya-pesnya-maynkraft.mp3')
-pygame.mixer.music.play()
-t = 0
-b = False
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.KEYUP:
-            if event.key == 275:
-                all_sprites.empty()
-                move = AnimatedSprite(load_image('anim1.png'), 8, 1, 0, 0)
-        if event.type == pygame.KEYDOWN:
-            if event.key == 275:
-                all_sprites.empty()
-                move = AnimatedSprite(load_image('anim2.png'), 9, 1, 0, 0)
-            if event.key == 32:
-                all_sprites.empty()
-                move = AnimatedSprite(load_image('anim3.png'), 7, 1, 0, 0)
-                b = True
-    if b:
-        t += 1
-    if t == 7:
-        b = False
-        t = 0
-        all_sprites.empty()
-        move = AnimatedSprite(load_image('anim1.png'), 8, 1, 0, 0)
-    screen.fill(pygame.Color('white'))
-    all_sprites.draw(screen)
-    all_sprites.update()
-    clock.tick(10)
-    pygame.display.flip()
-pygame.quit()
