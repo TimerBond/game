288aeafac35e1706edf77eab61f817129c9553b8
TimerBond <45458954+TimerBond@users.noreply.github.com>
2019-02-21 17:44:53 +0300

    Add files via upload

---
+++ pygame/ProjectPygame/anim.py
@@ -0,0 +1,28 @@
+import pygame
+
+
+def __init__(self, sheet, columns, rows, x, y, all_sprites, player_HP):
+    super().__init__(all_sprites)
+    self.frames = []
+    self.cut_sheet(sheet, columns, rows)
+    self.cur_frame = 0
+    self.image = self.frames[self.cur_frame]
+    self.rect = self.image.get_rect().move(
+        x, y - self.rect.h + CELL_SIZE + 3
+    )
+    self.HP = player_HP
+
+
+def cut_sheet(self, sheet, columns, rows):
+    self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
+                            sheet.get_height() // rows)
+    for j in range(rows):
+        for i in range(columns):
+            frame_location = (self.rect.w * i, self.rect.h * j)
+            self.frames.append(sheet.subsurface(pygame.Rect(
+                frame_location, self.rect.size)))
+
+
+def update(self):
+    self.cur_frame = (self.cur_frame + 1) % len(self.frames)
+    self.image = self.frames[self.cur_frame]
