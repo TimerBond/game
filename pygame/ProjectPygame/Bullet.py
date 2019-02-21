67b0bc428067a38873309cea38df4e877a54ba0a
bulat3103 <45459817+bulat3103@users.noreply.github.com>
2019-02-21 12:30:56 +0300

    Add files via upload

--- pygame/ProjectPygame/Bullet.py
+++ pygame/ProjectPygame/Bullet.py
@@ -2,6 +2,20 @@ import pygame
 
 
 class Bullet(pygame.sprite.Sprite):
+    image = pygame.image.load("sprites/fireball_1.png")
 
-    def __init__(self, group):
+
+    def __init__(self, group, x, y, direction):
         super().__init__(group)
+        self.image = Bullet.image
+        self.rect = self.image.get_rect().move(
+            x - 10, y
+        )
+        self.direction = direction
+        self.type = {
+            0: -5,
+            1: 5
+        }
+
+    def update(self, *args):
+        self.rect.x += self.type[self.direction]
