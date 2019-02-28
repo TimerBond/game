b1379b24325e32eab8bc147e0d09c6c3fa2d215d
bulat3103 <45459817+bulat3103@users.noreply.github.com>
2019-02-28 17:32:57 +0300

    Update Main.py

--- ProjectPygame/Main.py
+++ ProjectPygame/Main.py
@@ -126,7 +126,7 @@ while play:
 
     width, height, count_coins = generate_map(load_level(mapName))
     player = MainPlayer.AnimatedSprite(player_x, player_y, player_group, all_blocks, all_sprites, finish_group)
-    pygame.mixer.music.load('sounds/C418.mp3')
+    pygame.mixer.music.load('sounds/C418 - Subwoofer Lullaby.mp3')
     pygame.mixer.music.play()
     go = False
     clock = pygame.time.Clock()
