c2d1729da08cf2cb9092b7c23ce79f6b19779d05
bulat3103 <45459817+bulat3103@users.noreply.github.com>
2019-02-28 17:31:59 +0300

    Add files via upload

--- ProjectPygame/Main.py
+++ ProjectPygame/Main.py
@@ -14,6 +14,7 @@ import Finish
 import Menu
 
 
+pygame.init()
 def terminate():
     pygame.quit()
     sys.exit()
@@ -125,8 +126,8 @@ while play:
 
     width, height, count_coins = generate_map(load_level(mapName))
     player = MainPlayer.AnimatedSprite(player_x, player_y, player_group, all_blocks, all_sprites, finish_group)
-    #pygame.mixer.music.load('sounds/C418 - Subwoofer Lullaby.mp3')
-    #pygame.mixer.music.play()
+    pygame.mixer.music.load('sounds/C418.mp3')
+    pygame.mixer.music.play()
     go = False
     clock = pygame.time.Clock()
     runned = 0
