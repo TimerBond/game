e93c2c3602f4efff64bd8f9c56cd37fc9b5f30d7
TimerBond <45458954+TimerBond@users.noreply.github.com>
2019-03-03 20:13:18 +0300

    Add files via upload

---
+++ pygame/anim.py
@@ -0,0 +1,58 @@
+import os, pygame
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    try:
+        image = pygame.image.load(fullname)
+    except pygame.error as message:
+        print('Cannot load image:', name)
+        raise SystemExit(message)
+    image = image.convert_alpha()
+
+    if colorkey is not None:
+        if colorkey is -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    return image
+
+
+class AnimatedSprite(pygame.sprite.Sprite):
+    def __init__(self, sheet, columns, rows, x, y):
+        super().__init__(all_sprites)
+        self.frames = []
+        self.cut_sheet(sheet, columns, rows)
+        self.cur_frame = 0
+        self.image = self.frames[self.cur_frame]
+        self.rect = self.rect.move(x, y)
+
+    def cut_sheet(self, sheet, columns, rows):
+        self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
+                                sheet.get_height() // rows)
+        for j in range(rows):
+            for i in range(columns):
+                frame_location = (self.rect.w * i, self.rect.h * j)
+                self.frames.append(sheet.subsurface(pygame.Rect(
+                    frame_location, self.rect.size)))
+
+    def update(self):
+        self.cur_frame = (self.cur_frame + 1) % len(self.frames)
+        self.image = self.frames[self.cur_frame]
+
+pygame.init()
+size = width, height = 120, 120
+screen = pygame.display.set_mode(size)
+all_sprites = pygame.sprite.Group()
+afk = AnimatedSprite(load_image('anim1.png'), 8, 1, 0, 0)
+clock = pygame.time.Clock()
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    screen.fill(pygame.Color('white'))
+    all_sprites.draw(screen)
+    all_sprites.update()
+    clock.tick(15)
+    pygame.display.flip()
+pygame.quit()
